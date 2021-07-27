6c10ea9c99296ed253c96291e9bf4b14afbc4b56
xuan <xuan.xa@alibaba-inc.com>
2021-07-27 15:50:03 +0800

    bb

---
+++ mix/src/main/java/com/xuan/mix/bt/jijin/WangGe.java
@@ -0,0 +1,30 @@
+package com.xuan.mix.bt.jijin;
+
+/**
+ * 网格交易
+ *
+ * @author xuan
+ * @since 2021/6/13
+ */
+public class WangGe {
+    /**
+     * 初始仓位，单位元
+     */
+    private int chuShiCangWei = 5000;
+    /**
+     * 累计伐值，单位百分比
+     */
+    private int leiJiFaZhi = 2;
+    /**
+     * 固定投入，单位元
+     */
+    private int guDingTouRu = 1000;
+    /**
+     * 账户余额
+     */
+    private int zhangHuYuE = chuShiCangWei;
+
+    public static void main(String[] args) {
+
+    }
+}
