83ad3eb4842107d99ec9ee52ecc25e73e2e3d82a
xuan <xuan.xa@alibaba-inc.com>
2021-08-15 23:30:55 +0800

    bb

---
+++ mix/src/main/java/com/xuan/mix/design/strategy/Client.java
@@ -0,0 +1,18 @@
+package com.xuan.mix.design.strategy;
+
+/**
+ * 高层模块，用来执行不同的策略
+ *
+ * @author xuan
+ * @since 2021/8/15
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        StrategyContext context = new StrategyContext(new StrategyImpl1());
+        context.doSomething();
+
+        context = new StrategyContext(new StrategyImpl2());
+        context.doSomething();
+    }
+}
---
+++ mix/src/main/java/com/xuan/mix/design/strategy/StrategyImpl1.java
@@ -0,0 +1,15 @@
+package com.xuan.mix.design.strategy;
+
+/**
+ * 策略实现1
+ *
+ * @author xuan
+ * @since 2021/8/15
+ */
+public class StrategyImpl1 implements Strategy {
+
+    @Override
+    public void doSomething() {
+        System.out.println("我是策略实现1");
+    }
+}
