3376d999eaf6af0cfc87c2c920ddd991deabee8a
xuan <xuan.xa@alibaba-inc.com>
2021-09-04 00:00:00 +0800

    bb

--- spring-utils/src/main/java/com/xuan/spring/utils/nodelog/spi/Slf4jLoggerSpi.java
+++ spring-utils/src/main/java/com/xuan/spring/utils/nodelog/spi/Slf4jLoggerSpi.java
@@ -1,6 +1,6 @@
-package com.xuan.spring.utils.nodelog.spi.impl;
+package com.xuan.spring.utils.nodelog.spi;
 
-import com.xuan.spring.utils.nodelog.spi.LoggerSpi;
+import com.xuan.mix.bt.nodelog.spi.LoggerSpi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,12 +9,13 @@ import org.slf4j.LoggerFactory;
  * @since 2021/9/3
  */
 public class Slf4jLoggerSpi implements LoggerSpi {
-    
+
     private final static Logger log = LoggerFactory.getLogger("Slf4jLoggerSpi");
 
     @Override
     public void info(String msg) {
-        log.info(msg);
+        //log.info(msg);
+        System.out.println("hah:" + msg);
     }
 
 }
