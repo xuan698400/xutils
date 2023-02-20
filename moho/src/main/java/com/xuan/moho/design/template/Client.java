986816cb2f6f33b354fe2d1db4d07e7d656aecb4
xuan <xuan.xa@alibaba-inc.com>
2023-02-20 19:44:36 +0800

    bb

---
+++ moho/src/main/java/com/xuan/moho/design/template/Client.java
@@ -0,0 +1,18 @@
+package com.xuan.moho.design.template;
+
+/**
+ * @author xuan
+ * @since 2023/2/20
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+        Game game = new GameMovie();
+        System.out.println("----看电影----");
+        game.play();
+        game = new GameCamping();
+        System.out.println("----去露营----");
+        game.play();
+    }
+}
---
+++ moho/src/main/java/com/xuan/moho/design/template/GameCamping.java
@@ -0,0 +1,13 @@
+package com.xuan.moho.design.template;
+
+/**
+ * @author xuan
+ * @since 2023/2/20
+ */
+public class GameCamping extends Game {
+
+    @Override
+    protected void doPlay() {
+        System.out.println("露营真好玩");
+    }
+}
